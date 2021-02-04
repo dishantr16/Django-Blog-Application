f5cf573033b7910e7c81202a3974bb1cfe0bb6bd
Dishant Rathi <dishant16rathi@gmail.com>
2021-02-04 19:15:10 +0530

    Coverage testing performed in blog_api

--- core/blog_api/tests.py
+++ core/blog_api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTest(APITestCase):
+
+    def test_view_posts(self):
+
+        url = reverse('blog_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = {"title": "new", "author": 1,
+                "excerpt": "new", "content": "new"}
