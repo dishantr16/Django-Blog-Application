f7ae6129d6d032ec4a8621f361ae2aaf78fa16e4
Dishant Rathi <dishant16rathi@gmail.com>
2021-02-04 23:14:58 +0530

    created isauthenticated and permission roles in blogapi and coverage tested successful

--- core/core/settings.py
+++ core/core/settings.py
@@ -126,9 +126,15 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+# Permissions:
+# AllowAny
+# IsAuthenticated
+# IsAdminUser
+# IsAuthenticatedOrReadOnly
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
